3b369729ba272487b1f82c0568fc9fc25b6ff1ef
Raffaello Fraboni <s22627@iisve.it>
2019-12-05 15:44:18 +0100

    Fixed 23

--- round1/project/src/23/main.cpp
+++ round1/project/src/23/main.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
 #include <vector>
 
+constexpr auto CACHE_INTERVAL = 10;
 constexpr auto MAXN = 100000000;
 constexpr auto MAX_BASE2 = 16384;
 constexpr auto MAX_BASE3 = 19683;
 
-std::vector<int> base2(MAX_BASE2 + 1);
-std::vector<int> base3(MAX_BASE3 + 1);  
+int base2[MAX_BASE2 + 1];
+int base3[MAX_BASE3 + 1];
+uint32_t cache[MAXN / CACHE_INTERVAL];
+size_t maxIndex = 0;
+
+int countSpecialNumbers(uint32_t N)
+{
+    size_t count;
+    uint32_t i;
+    if (maxIndex > N / CACHE_INTERVAL)
+    {
+        count = cache[N / CACHE_INTERVAL];
+        i = N / CACHE_INTERVAL * CACHE_INTERVAL + 1;
+    }
+    else
+    {
+        count = cache[maxIndex];
+        i = maxIndex * CACHE_INTERVAL + 1;
+    }
+
+    for (; i <= N; i++)
+    {
+        int bits2 = base2[i / MAX_BASE2] + base2[i % MAX_BASE2];
+        int bits3 = base3[i / MAX_BASE3] + base3[i % MAX_BASE3];
+        if (bits2 == bits3)
+            ++count;
+
+        if (i % CACHE_INTERVAL == 0)
+        {
+            cache[i / CACHE_INTERVAL] = count;
+            maxIndex = i / CACHE_INTERVAL;
+        }
+    }
+    return count;
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,16 +57,7 @@ int main(int argc, char *argv[])
         uint32_t N;
         std::cin >> N;
 
-        uint32_t count{};
-        for (int i = 1; i <= N; i++)
-        {
-            int bits2 = base2[i / MAX_BASE2] + base2[i % MAX_BASE2];
-            int bits3 = base3[i / MAX_BASE3] + base3[i % MAX_BASE3];
-            if (bits2 == bits3)
-                ++count;
-        }
-
-        std::cout << count << ' ';
+        std::cout << countSpecialNumbers(N) << ' ';
     }
 
     return 0;
