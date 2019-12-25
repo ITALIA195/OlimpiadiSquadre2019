ec4f6dd45b7152f5458059e9c99613b170db8edd
Raffaello Fraboni <s22627@iisve.it>
2019-12-25 21:10:41 +0100

    Replaced manual ordered vector with a multiset

--- round1/project/src/blindpunch/main.cpp
+++ round1/project/src/blindpunch/main.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
+#include <set>
 #include <iomanip>
-
-int binarySearch(std::vector<std::pair<double, double>> vec, double num)
-{
-    int left = 0;
-    int right = (int)vec.size() - 1;
-    while (left <= right)
-    {
-        auto median = left + (right - left >> 1);
-        auto pivot = vec[median].first;
-
-        if (num == pivot)
-            return num;
-        if (num < pivot)
-            right = median - 1;
-        if (num > pivot)
-            left = median + 1;
-    }
-    // When not found return insert index
-    return left;
-}
 
 int main(int argc, char *argv[])
 {
@@ -32,27 +12,26 @@ int main(int argc, char *argv[])
         int N, K;
         std::cin >> N >> K;
 
-        std::vector<std::pair<double, double>> inputs;
+        std::multiset<std::pair<double, double>, std::greater<std::pair<double, double>>> values;
         for (size_t i = 0; i < N; i++)
         {
             double P;
             std::cin >> P;
             
-            int index = binarySearch(inputs, P);
-            inputs.insert(inputs.begin() + index, { P, 1 - P });
+            values.emplace(P, 1 - P);
         }
         
         double probTot{};
         for (size_t i = 0; i < K; i++)
         {
-            auto item = inputs.back();
-            inputs.pop_back();
+            auto it = values.begin();
+            auto item = *it;
 
             probTot += item.first;
             item.first *= item.second;
 
-            int index = binarySearch(inputs, item.first);
-            inputs.insert(inputs.begin() + index, item);
+            values.erase(it);
+            values.emplace(item);
         }
 
         probTot = floor(probTot * 1e6) / 1e6;
